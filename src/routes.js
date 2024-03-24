f807720ddb5602cdf6d38b1a2ef448493eccfed8
Reginaldo Lima <108771153+eusouregislima@users.noreply.github.com>
2024-03-24 19:40:56 -0300

    controllers ans migrations

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SEssionController';
 
 const routes = new Router();
 
-routes.get('/', (request, response) => {
-  return response.status(200).json({ message: 'Hello Node' });
-});
+routes.post('/users', UserController.store);
+routes.post('/session', SessionController.store);
 
 export default routes;
